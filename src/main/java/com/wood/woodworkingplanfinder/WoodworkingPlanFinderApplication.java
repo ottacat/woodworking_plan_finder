b6ed24da10c602885ca94a4605fa1bd9af361574
Timothy Shouldice <timothyshouldice@gmail.com>
2024-03-06 08:30:07 -0500

    Initial commit

---
+++ src/main/java/com/wood/woodworkingplanfinder/WoodworkingPlanFinderApplication.java
@@ -0,0 +1,13 @@
+package com.wood.woodworkingplanfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodworkingPlanFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodworkingPlanFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wood/woodworking_plan_finder/WoodworkingPlanFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wood.woodworking_plan_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodworkingPlanFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
